29cf4cd9df8b2a1dbfa5b9a0b128f86ea826d1bc
Luka Senicic <lukashadow23@gmail.com>
2025-07-10 22:35:24 +0200

    Manual resolve of conflict

--- csharp/dominoes/Dominoes.cs
+++ csharp/dominoes/Dominoes.cs
@@ -1,19 +1,10 @@
-using System.Reflection.Metadata.Ecma335;
-
 public static class Dominoes
 {
     public static bool CanChain(IEnumerable<(int, int)> dominoes)
     {
         var dominoesList = dominoes.ToList();
         if (dominoesList.Count == 0) return true;
-<<<<<<< HEAD
-<<<<<<< HEAD
         
-=======
->>>>>>> 0ae2a71 (Second backup)
-=======
-        
->>>>>>> da43846 (Changed dominoes)
         var start = dominoesList[0];
         var rest = dominoesList.Skip(1).ToList();
 
@@ -25,8 +16,6 @@ public static class Dominoes
         if (remaining.Count == 0)
             return current == start;
         
-<<<<<<< HEAD
-<<<<<<< HEAD
         for (int i = 0; i < remaining.Count; i++)
         {
             var domino = remaining[i];
@@ -43,24 +32,8 @@ public static class Dominoes
                 if (CanChainRecursive(domino.Item1, rest, start))
                     return true;
             }
-<<<<<<< HEAD
         }
 
         return false;
     }
 }
-=======
-        foreach (var(left,right) in dominoesList)
-=======
-        for (int i = 0; i < remaining.Count; i++)
->>>>>>> bd2b272 (same)
-        {
-            
-=======
->>>>>>> da43846 (Changed dominoes)
-        }
-
-        return false;
-    }
-}
->>>>>>> 0ae2a71 (Second backup)
