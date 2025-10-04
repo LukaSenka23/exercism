e713544d4bc0e1f62a993715c2035ca60eb831df
Luka Senicic <lukashadow23@gmail.com>
2025-10-04 20:34:01 +0200

    Added error handling

---
+++ csharp/error-handling/ErrorHandling.cs
@@ -0,0 +1,25 @@
+public static class ErrorHandling
+{
+    public static void HandleErrorByThrowingException()
+    {
+        throw new Exception();
+    }
+
+    public static int? HandleErrorByReturningNullableType(string input)
+    {
+        if (int.TryParse(input, out int number))
+            return number;
+        return null;
+    }
+
+    public static bool HandleErrorWithOutParam(string input, out int result)
+    {
+        return int.TryParse(input, out result);
+    }
+
+    public static void DisposableResourcesAreDisposedWhenExceptionIsThrown(IDisposable disposableObject)
+    {
+        using (disposableObject)
+            throw new Exception();
+    }
+}
